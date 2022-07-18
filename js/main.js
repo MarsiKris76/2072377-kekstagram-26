007dda4921476244a7dc9eb1eb1ba8d710c7ce6e
Быков Максим <bykovme@krista.ru>
2022-07-18 15:51:02 +0300

    Изменил функцию получения случайного числа на рекомендованную курсом.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,8 @@
-/*
-Готовая функция с сайта MDN:
-https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#получение_случайного_целого_числа_в_заданном_интервале_включительно
-*/
-function getRandomIntInclusive(min, max) {
-  if (min > max) {
-    return; // вернёт "undefined"
-  }
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomIntInclusive (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 }
 
 getRandomIntInclusive();
